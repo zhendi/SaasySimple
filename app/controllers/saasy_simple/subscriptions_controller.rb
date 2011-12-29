d531abab1124313d70c83b404c09f9ccf7ef0da2
Jonathan Jeffus <jjeffus@gmail.com>
2011-12-29 10:19:48 -0600

    [b] need to fix some names

--- app/controllers/saasy_simple/subscriptions_controller.rb
+++ app/controllers/saasy_simple/subscriptions_controller.rb
@@ -1,13 +1,16 @@
 module SaasySimple
   class SubscriptionsController < ApplicationController
     def activate
+      logger.info "Activate: "+params.inspect
     end
-  
+
     def billing
+      logger.info "Billing: "+params.inspect
+      redirect_to 'http://sites.fastspring.com/albumdraft/product/albumdraft?referrer='+current_user.id
     end
-  
+
     def deactivate
+      logger.info "Deactivate: "+params.inspect
     end
-  
   end
 end
