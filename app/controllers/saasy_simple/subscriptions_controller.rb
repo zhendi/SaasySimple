303027f82c7ae49b3bb161572fc0e76c6622ccd7
Jonathan Jeffus <jjeffus@gmail.com>
2012-01-07 10:14:11 -0600

    [f] finished refactoring and fixed bugs

--- app/controllers/saasy_simple/subscriptions_controller.rb
+++ app/controllers/saasy_simple/subscriptions_controller.rb
@@ -4,43 +4,24 @@ require 'open-uri'
 module SaasySimple
   class SubscriptionsController < ApplicationController
     def activate
-      logger.info "Activate: "+params.inspect
       if Digest::MD5.hexdigest(params["security_data"] + SaasySimple.config.secret) == params["security_hash"]
-        logger.info "Success: Validated"
         SaasySimple.config.model.activate( params['token'], params['id'] )
-      else
-        logger.info "activate: Failed: Validation"
       end
     end
 
     def billing
       return unless current_user
       if current_user.status == 'active'
-        x = open(
-          "https://api.fastspring.com/company/" +
-          SaasySimple.config.store_id +
-          "/subscription/" + current_user.token +
-          "?user=" + SaasySimple.config.username +
-          "&pass=" + SaasySimple.config.password
-        )
-        xml = x.read
-        nok = Nokogiri::XML(xml)
-        @nextPeriodDate = nok.xpath("//nextPeriodDate").text
-        @status = nok.xpath("//status").text
-        @customerUrl = nok.xpath("//customerUrl").text
-        render SaasySimple.config.view
+        subscription = SaasySimple.subscription(current_user)
+        redirect_to subscription['customerUrl']
       else
-        redirect_to "#{SaasySimple.config.url}?referrer=#{current_user.id}"
+        redirect_to SaasySimple.signup(current_user)
       end
     end
 
     def deactivate
-      logger.info "Deactivate: "+params.inspect
       if Digest::MD5.hexdigest(params["security_data"] + APP.config.secret) == params["security_hash"]
-        logger.info "Success: Validated"
         SaasySimple.config.model.deactivate( params['token'], params['id'] )
-      else
-        logger.info "activate: Failed: Validation"
       end
     end
   end
--- lib/SaasySimple.rb
+++ lib/SaasySimple.rb
@@ -8,4 +8,26 @@ module SaasySimple
 
     return @@config
   end
+
+  def self.subscription(user)
+    s = {}
+    if user.status == 'active'
+      x = open(
+        "https://api.fastspring.com/company/" +
+        SaasySimple.config.store_id +
+        "/subscription/" + user.token +
+        "?user="         + SaasySimple.config.username +
+        "&pass="         + SaasySimple.config.password
+      )
+      xml = x.read
+      nok = Nokogiri::XML(xml)
+      s['nextPeriodDate'] = nok.xpath("//nextPeriodDate").text if nok.xpath("//nextPeriodDate")
+      s['status']         = nok.xpath("//status").text         if nok.xpath("//status")
+      s['customerUrl']    = nok.xpath("//customerUrl").text    if nok.xpath("//customerUrl")
+    end
+    s
+  end
+  def self.signup(user)
+    "#{SaasySimple.config.url}?referrer=#{user.id}"
+  end
 end
