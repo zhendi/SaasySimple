5c35b95979160fe4d5152d6250a2e8f8ac0aa553
zhendi <zhendiwang@gmail.com>
2012-03-28 15:21:39 +0800

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-SaasySimple::Engine.routes.draw do
+Rails.application.routes.draw do
   post "subscriptions/activate",   :controller => 'subscriptions', :action => 'activate'
   get  "subscriptions/billing",    :controller => 'subscriptions', :action => 'billing'
   post "subscriptions/deactivate", :controller => 'subscriptions', :action => 'deactivate'
