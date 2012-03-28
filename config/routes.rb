acbc3f3ef5130fc69efeaaf5b4c5db064d6e7212
zhendi <zhendiwang@gmail.com>
2012-03-28 17:08:44 +0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :saasy do
+  namespace :saasy_simple do
     post "subscriptions/activate",   :controller => 'subscriptions', :action => 'activate'
     get  "subscriptions/billing",    :controller => 'subscriptions', :action => 'billing'
     post "subscriptions/deactivate", :controller => 'subscriptions', :action => 'deactivate'
